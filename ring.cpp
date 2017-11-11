737c99881fbbd063c74363fff188435cadbd560f
Carolina <krito_j_g@utp.edu.co>
2017-11-11 17:15:43 -0500

    Need tests

--- ring.cpp
+++ ring.cpp
@@ -34,81 +34,103 @@ int toInt(string s) {
   return out;
 }
 
-// TODO
-string getIp() {
-	return "";
-}
 
 int main(int argc, char** argv) {
 
-	if(argc != 3){
-		cout << "Missing arguments" << endl;
+	if(argc != 5){
+		cout << "Usage: \"<local ip>\" \"<local port>\" \"<remote ip>\" \"<remote port>\"" << endl;
 		return 1;
 	}
 
-	string port = "555" + string(argv[1]);
-	string server_port = "tcp://*:" + port;
-	string client_port = "tcp://localhost:555" + string(argv[2]);
+	string myIp(argv[1]), myPort(argv[2]), ipSucessor(argv[3]), portSucessor(argv[4]);
+	string ipPrevious = "", portPrevious = "";
+
+	string tcp = "tcp://";
+	string server_endPoint = tcp + myIp + ":" + myPort; // e.g: "tcp://*:5555";
+	string client_endPoint = tcp + ipSucessor + ":" + portSucessor; // e.g: "tcp://localhost:5555";
+	string previous_endPoint = tcp + ipPrevious + ":" + portPrevious;
 
   context ctx;
 	socket s_server(ctx, socket_type::rep); //Listening
 	socket s_client(ctx, socket_type::req); //Asking
 
-  s_server.bind(server_port);
-  s_client.connect(client_port);
-  cout << "Server listening on " << server_port << endl;
-  cout << "Client connected on " << client_port << endl;
+  s_server.bind(server_endPoint);
+	cout << "Server listening on " << server_endPoint << endl;
+  s_client.connect(client_endPoint);
+	cout << "Client connected on " << client_endPoint << endl;
 
   poller pol;
   pol.add(s_server);
   pol.add(s_client);
 
-  int myId = getRandom(), sucessorId;
+  int myId = getRandom(), sucessorId, predecessorId;
+	string ipLookingFor = "";
 	dbg(myId);
 
 	int i = 0;
 
-	string myIp = getIp(), ipSucessor; //ip's
-	bool id_flag = false, other_flag = false;
+	bool id_flag = false;
 
 	message m;
 	m << "What's your ID?";
 	s_client.send(m);
 
   while (true) {
+		dbg(i);
     if (pol.poll()) {
-      if (pol.has_input(s_client)) {
+
+			if (pol.has_input(s_client)) {
+
 				message m, n;
-				string ans, server_id, server_ip, s_port;
+				string ans, server_predecessor_id, server_id;
         s_client.receive(m);
         m >> ans;
 				cout << "Receiving from server -> " << ans << endl;
 
 				if (ans == "My ID is") {
-					m >> server_id;
-					cout << " " << server_id << endl;
-					sucessorId = toInt(server_id);
-					id_flag = true;
+					m >> server_id >> server_predecessor_id;
+					dbg(server_id);
+					dbg(server_predecessor_id);
+
+					if (myId < toInt(server_id) and myId > toInt(server_predecessor_id)) {
+						// connect between server_predecessor_id and server_id
+						predecessorId = toInt(server_predecessor_id);
+						sucessorId = toInt(server_id);
+						n << "Now I am your predecessor" << toString(myId);
+						s_client.send(n);
+						previous_endPoint = tcp + ipPrevious + ":" + portPrevious;
+						s_client.connect(previous_endPoint);
+						message l;
+						l << "Now I am your sucessor" << myIp << myPort;
+						s_client.send(l);
+						s_client.connect(client_endPoint);
+
+					} else { // keep going through the ring
+						n << "What's your sucessor IP and PORT";
+						s_client.send(n);
+						id_flag = true;
+					}
 				}
-				if (ans == "My IP and PORT is") {
-					m >> server_ip;
-					m >> s_port;
-					cout << " " << server_ip << " : " << s_port << endl;
-					ipSucessor = server_ip;
-					server_port = s_port;
+				if (ans == "My sucessor IP and PORT is") {
+					ipPrevious = ipSucessor;
+					portPrevious = portSucessor;
+					m >> ipSucessor;
+					m >> portSucessor;
+					cout << ipSucessor << ":" << portSucessor << endl;
+					// connect
+					client_endPoint = tcp + ipSucessor + ":" + portSucessor;
+					s_client.connect(client_endPoint);
+					id_flag = false;
 				}
 
 				if (!id_flag) {
 					n << "What's your ID?";
 					s_client.send(n);
 				}
-				if (id_flag and !other_flag){
-					n << "What's your IP and PORT?";
-					s_client.send(n);
-					other_flag = true;
-				}
       }
+
       if (pol.has_input(s_server)) {
+
 				string ans;
 				message m, n;
         s_server.receive(m);
@@ -116,24 +138,27 @@ int main(int argc, char** argv) {
 				cout << "Receiving from client -> " << ans << endl;
 
 				if (ans == "What's your ID?") {
-					n << "My ID is" << toString(myId);
+					n << "My ID is" << toString(myId) << toString(predecessorId);
 					s_server.send(n);
 				}
-				if (ans == "What's your IP and PORT?") {
-					n <<  "My IP and PORT is" << myIp << port;
+				if (ans == "What's your sucessor IP and PORT") {
+					n << "My sucessor IP and PORT is" << ipSucessor << portSucessor;
 					s_server.send(n);
 				}
-				// if (ans == "IP predecessor") {
-				// 	m >> predecessor_ip;
-				// 	cout << " " << predecessor_ip << endl;
-				// 	ipPredecessor = predecessor_ip;
-				// 	ipPredecessor = true;
-				// }
-
+				if (ans == "Now I am your predecessor") {
+					string ans2;
+					m >> ans2;
+					predecessorId = toInt(ans2);
+				}
+				if (ans == "Now I am your sucessor") {
+					m >> ipSucessor >> portSucessor;
+					client_endPoint = tcp + ipSucessor + ":" + portSucessor;
+					s_client.connect(client_endPoint);
+				}
       }
     }
-		// i++;
-		// if (i == 6) break;
+		i++;
+		if (i == 20) break;
   }
 
 	return 0;
