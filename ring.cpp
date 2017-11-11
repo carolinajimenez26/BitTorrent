67d5d2f2998a5e3e2a5ac45f56ed9acdb50f03b5
Carolina <krito_j_g@utp.edu.co>
2017-11-11 11:06:44 -0500

    Add poller

--- ring.cpp
+++ ring.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "lib/node.hh"
 #include <zmqpp/zmqpp.hpp>
 
 using namespace std;
@@ -7,42 +6,44 @@ using namespace zmqpp;
 
 #define dbg(x) cout << #x << ": " << x << endl
 
-// class node
-// {
-// private:
-// 	int myid;
-// 	int predecessor;
-// 	int sucessor;
-// 	string ipSucessor, ipPredecessor; //ip's
-// 	string currentNode, nextNode; //ip's
-// 	context ctx;
-// 	socket s_server(ctx, socket_type::rep); //Listening
-// 	socket s_client(ctx, socket_type::req); //Asking
-// 	poller pol;
-// public:
-// 	node(string server_port, string client_port){
-// 		s_server.bind(server_port);
-// 		s_client.connect(client_port);
-// 		cout << "Server listening on " << server_port << endl;
-// 		cout << "Client connected on " << client_port << endl;
-// 		pol.add(s_server);
-// 		pol.add(s_client);
-//
-// 	}
-// };
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
 
 	if(argc != 3){
 		cout << "Missing arguments" << endl;
-		return -1;
+		return 1;
 	}
 	string a(argv[1]);
 	string b(argv[2]);
 	string server_port = "tcp://*:555" + a;
 	string client_port = "tcp://localhost:555" + b;
-	// node n(server_port, client_port);
+
+  context ctx;
+	socket s_server(ctx, socket_type::rep); //Listening
+	socket s_client(ctx, socket_type::req); //Asking
+
+  s_server.bind(server_port);
+  s_client.connect(client_port);
+  cout << "Server listening on " << server_port << endl;
+  cout << "Client connected on " << client_port << endl;
+
+  poller pol;
+  pol.add(s_server);
+  pol.add(s_client);
+
+  int myid, predecessor, sucessor;
+	string ipSucessor, ipPredecessor, currentNode, nextNode; //ip's
+
+  while (true) {
+    if (pol.poll()) {
+      if (pol.has_input(s_client)) {
+
+      }
+      if (pol.has_input(s_server)) {
+
+      }
+    }
+  }
 
 	return 0;
 }
